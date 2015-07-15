ee497696a8b76c49727049ecca7af97181e15317
Steven Smith <stevensmith@Stevens-MacBook-Air.local>
2015-07-14 18:11:09 -0600

    got gulp to transpile es6 to es5.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var babel = require("gulp-babel");
 
-gulp.task('default', function() {
-    console.log('potatoes');
+//TODO why do I have to specify the path in two places?
+
+gulp.task("default", function () {
+  return gulp.src("public/src/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/build"));
+});
+
+gulp.task("watch", function(){
+    gulp.watch('public/src/*.js', ['default']);
 });
