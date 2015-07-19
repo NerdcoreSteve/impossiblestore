bf39458b874f4d8b9e77e5ea37734ae1dee29483
Steven Smith <stevensmith@Stevens-MacBook-Air.local>
2015-07-19 13:55:53 -0600

    added favicon code and icon

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var favicon = require('serve-favicon');
+
+app.use(favicon('public/images/favicon.ico'));
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
